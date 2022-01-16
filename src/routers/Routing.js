988cda6b3fd20a1685bb05d1ac6e836a0797f6b6
Rashomon <ericm@abyssa.com>
2022-01-16 20:44:28 +0100

    minor adjust

--- src/routers/Routing.js
+++ src/routers/Routing.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import { RecipeView } from '../view/RecipeView'
 import { HomeView } from '../view/HomeView'
 import { SignInView } from '../view/SignInView'
 
 export const Routing = (props) => {
     return (
-        <Router>
+        <BrowserRouter>
             {props.children}
                 <Route exact path="/recipe" component={RecipeView} />
                 <Route exact path="/signin" component={SignInView} />
                 <Route component={HomeView} />
-        </Router>
+        </BrowserRouter>
     )
 }
